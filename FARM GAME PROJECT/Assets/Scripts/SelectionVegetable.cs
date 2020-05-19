e1d3bdf764b0095751597479522742ed78476de5
PITARA <pitarapey@gmail.com>
2020-05-19 02:41:03 -0300

    Object Highlight
    Object Highlight added

---
+++ FARM GAME PROJECT/Assets/Scripts/SelectionVegetable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionVegetable : MonoBehaviour
+{
+    #region Variables
+    private Renderer rend;
+
+    private float defaultOutline = 1.0f;
+    private float highlightedOutline = 1.05f;
+    #endregion
+
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Outline");
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.SetFloat("_OutlineWidth", highlightedOutline);
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.SetFloat("_OutlineWidth", defaultOutline);
+    }
+}
