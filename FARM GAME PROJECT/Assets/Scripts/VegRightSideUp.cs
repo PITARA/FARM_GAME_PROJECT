6c15c53261f880e869ae7ecbbc8dcefac61362f3
PITARA <pitarapey@gmail.com>
2020-06-17 19:08:27 -0300

    Vegetable Right Side Up
    Vegetable goes right ride up and faces the player when it hits the floor

---
+++ FARM GAME PROJECT/Assets/Scripts/VegRightSideUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegRightSideUp : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("MainCamera");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if vegetable collides with floor
+        if (collision.gameObject.tag == "Floor")
+        {
+            // Vegetable will stand right side up
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            gameObject.GetComponent<Transform>().LookAt(player.GetComponent<Transform>());
+        }
+    }
+}
